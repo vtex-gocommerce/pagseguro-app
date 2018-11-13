acb6f450b47468b888f9d9ac706bd4ed72b70d8d
Gustavo Rosolem <guspace92@hotmail.com>
2018-11-13 13:20:16 -0200

    Init react

---
+++ react/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+interface IAppProps {
+  children: React.ReactElement<any>
+}
+
+const Pagseguro: React.SFC<IAppProps> = props => {
+  // return props.children
+  return <div>HAHAHAHA</div>
+}
+
+export default Pagseguro
